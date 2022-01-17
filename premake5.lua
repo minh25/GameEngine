349423e44954c3f3376f759ff0341c9c4c37a821
minh25 <minh00012258@gmail.com>
2022-01-17 10:15:29 +0700

    add premake

---
+++ premake5.lua
@@ -0,0 +1,106 @@
+workspace "GameEngine"
+	architecture "x64"
+	startproject "Sandbox"
+
+	configurations
+	{
+		"Debug",
+		"Release",
+		"Dist"
+	}
+
+outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
+
+project "Sandbox"
+	location "Sandbox"
+	kind "ConsoleApp"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"GameEngine/vendor/spdlog/include",
+		"GameEngine/src"
+	}
+
+	links
+	{
+		"GameEngine"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"ENGINE_PLATFORM_WINDOWS",
+		}
+
+	filter "configurations:Debug"
+		defines "ENGINE_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "ENGINE_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "ENGINE_DIST"
+		symbols "On"
+
+project "GameEngine"
+	location "GameEngine"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"ENGINE_PLATFORM_WINDOWS",
+			"ENGINE_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "ENGINE_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "ENGINE_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "ENGINE_DIST"
+		symbols "On"
