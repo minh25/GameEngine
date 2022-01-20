b9c752064655ed3ca80170afce81df181001aab5
minh25 <minh00012258@gmail.com>
2022-01-21 01:38:44 +0700

    add event

--- premake5.lua
+++ premake5.lua
@@ -11,6 +11,54 @@ workspace "GameEngine"
 
 outputdir = "%{cfg.buildcfg}-%{cfg.system}-%{cfg.architecture}"
 
+project "GameEngine"
+	location "GameEngine"
+	kind "SharedLib"
+	language "C++"
+
+	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
+	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
+
+	files
+	{
+		"%{prj.name}/src/**.h",
+		"%{prj.name}/src/**.cpp"
+	}
+
+	includedirs
+	{
+		"%{prj.name}/src",
+		"%{prj.name}/vendor/spdlog/include"
+	}
+
+	filter "system:windows"
+		cppdialect "C++17"
+		staticruntime "On"
+		systemversion "latest"
+
+		defines
+		{
+			"ENGINE_PLATFORM_WINDOWS",
+			"ENGINE_BUILD_DLL"
+		}
+
+		postbuildcommands
+		{
+			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
+		}
+
+	filter "configurations:Debug"
+		defines "ENGINE_DEBUG"
+		symbols "On"
+
+	filter "configurations:Release"
+		defines "ENGINE_RELEASE"
+		optimize "On"
+
+	filter "configurations:Dist"
+		defines "ENGINE_DIST"
+		symbols "On"
+
 project "Sandbox"
 	location "Sandbox"
 	kind "ConsoleApp"
@@ -44,53 +92,6 @@ project "Sandbox"
 		defines
 		{
 			"ENGINE_PLATFORM_WINDOWS",
-		}
-
-	filter "configurations:Debug"
-		defines "ENGINE_DEBUG"
-		symbols "On"
-
-	filter "configurations:Release"
-		defines "ENGINE_RELEASE"
-		optimize "On"
-
-	filter "configurations:Dist"
-		defines "ENGINE_DIST"
-		symbols "On"
-
-project "GameEngine"
-	location "GameEngine"
-	kind "SharedLib"
-	language "C++"
-
-	targetdir ("bin/" .. outputdir .. "/%{prj.name}")
-	objdir ("bin-int/" .. outputdir .. "/%{prj.name}")
-
-	files
-	{
-		"%{prj.name}/src/**.h",
-		"%{prj.name}/src/**.cpp"
-	}
-
-	includedirs
-	{
-		"%{prj.name}/vendor/spdlog/include"
-	}
-
-	filter "system:windows"
-		cppdialect "C++17"
-		staticruntime "On"
-		systemversion "latest"
-
-		defines
-		{
-			"ENGINE_PLATFORM_WINDOWS",
-			"ENGINE_BUILD_DLL"
-		}
-
-		postbuildcommands
-		{
-			("{COPY} %{cfg.buildtarget.relpath} ../bin/" .. outputdir .. "/Sandbox")
 		}
 
 	filter "configurations:Debug"
