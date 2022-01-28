2620f6c7098d27d95b20bf6f30df2b985f9d0585
minh25 <minh00012258@gmail.com>
2022-01-28 16:26:46 +0700

    Window event

--- GameEngine/src/Engine/Application.cpp
+++ GameEngine/src/Engine/Application.cpp
@@ -1,21 +1,31 @@
 #include "enginepch.h"
 #include "Application.h"
 
-#include "Engine/Events/ApplicationEvent.h"
 #include "Engine/Log.h"
 
 #include <GLFW/glfw3.h>
 
 namespace Engine {
 
+#define BIND_EVENT_FN(x) std::bind(&x, this, std::placeholders::_1)
+
 	Application::Application()
 	{
 		m_Window = std::unique_ptr<Window>(Window::Create());
+		m_Window->SetEventCallback(BIND_EVENT_FN(Application::OnEvent));
 	}
 
 	Application::~Application()
 	{
 
+	}
+
+	void Application::OnEvent(Event& e)
+	{
+		EventDispatcher dispatcher(e);
+		dispatcher.Dispatch<WindowCloseEvent>(BIND_EVENT_FN(Application::OnWindowClose));
+
+		ENGINE_CORE_TRACE("{0}", e);
 	}
 
 	void Application::Run()
@@ -26,6 +36,13 @@ namespace Engine {
 			glClear(GL_COLOR_BUFFER_BIT);
 			m_Window->OnUpdate();
 		}
+	}
+
+	bool Application::OnWindowClose(WindowCloseEvent& e)
+	{
+		m_Running = false;
+
+		return true;
 	}
 
 }
