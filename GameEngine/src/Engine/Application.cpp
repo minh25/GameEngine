9417a8cb5084ebde02ebd81d8560181719b8831a
minh25 <minh00012258@gmail.com>
2022-05-12 01:36:34 +0700

    Input polling

--- GameEngine/src/Engine/Application.cpp
+++ GameEngine/src/Engine/Application.cpp
@@ -5,6 +5,8 @@
 
 #include <glad/glad.h>
 
+#include "Input.h"
+
 namespace Engine {
 
 #define BIND_EVENT_FN(x) std::bind(&x, this, std::placeholders::_1)
@@ -58,6 +60,10 @@ namespace Engine {
 
 			for (Layer* layer : m_LayerStack)
 				layer->OnUpdate();
+
+
+			auto [x, y] = Input::GetMousePosition();
+			ENGINE_CORE_TRACE("{0}, {1}", x, y);
 
 			m_Window->OnUpdate();
 		}
